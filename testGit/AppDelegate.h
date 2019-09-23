8b0c17ba9bd61af9edbc32505d814f3cb106dfb7
chaojie <514572490@qq.com>
2019-09-23 11:28:54 +0800

    diyici

---
+++ testGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  testGit
+//
+//  Created by 王超杰 on 2019/9/23.
+//  Copyright © 2019 wangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ testGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  testGit
+//
+//  Created by 王超杰 on 2019/9/23.
+//  Copyright © 2019 wangchaojie. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
